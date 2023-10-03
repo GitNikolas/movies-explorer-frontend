6aeef16ab6610a99ae2f85d595724641e7b94343
Nikolai Panteleev <panteleew.kolya@gmail.com>
2023-10-03 11:56:39 +0900

    Переименовал классы в соответствии с ТЗ

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from 'react-router-dom';
-import lanlogo from '../../images/text__COLOR_landing-logo.svg'
+import promologo from '../../images/text__COLOR_landing-logo.svg'
 
 function Main() {
 
@@ -7,45 +7,45 @@ function Main() {
     <main
     className="main"
     >
-      <section className="hero">
-        <h1 className="hero__title">Учебный проект студента факультета Веб-разработки.</h1>
-        <img className="hero__landing-logo" alt="Логотип страницы" src={lanlogo}/>
+      <section className="promo">
+        <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+        <img className="promo__landing-logo" alt="Логотип страницы" src={promologo}/>
       </section>
 
       <section
-      className="about"
+      className="aboutProject"
       >
-        <h2 className="about__title">О проекте</h2>
-        <article className="about__table">
-          <h3 className="about__table-title">Дипломный проект включал 5 этапов</h3>
-          <h3 className="about__table-title">На выполнение диплома ушло 5 недель</h3>
-          <p className="about__table-subtitle">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
-          <p className="about__table-subtitle">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        <h2 className="aboutProject__title">О проекте</h2>
+        <article className="aboutProject__table">
+          <h3 className="aboutProject__table-title">Дипломный проект включал 5 этапов</h3>
+          <h3 className="aboutProject__table-title">На выполнение диплома ушло 5 недель</h3>
+          <p className="aboutProject__table-subtitle">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+          <p className="aboutProject__table-subtitle">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
         </article>
 
-        <div className="about__illustrarion">
-        <p className="about__illustrarion-title about__illustrarion-title_type_1week">1 неделя</p>
-        <p className="about__illustrarion-title about__illustrarion-title_type_4week">4 недели</p>
-        <p className="about__illustrarion-subtitle">Back-end</p>
-        <p className="about__illustrarion-subtitle">Front-end</p>
+        <div className="aboutProject__illustrarion">
+        <p className="aboutProject__illustrarion-title aboutProject__illustrarion-title_type_1week">1 неделя</p>
+        <p className="aboutProject__illustrarion-title aboutProject__illustrarion-title_type_4week">4 недели</p>
+        <p className="aboutProject__illustrarion-subtitle">Back-end</p>
+        <p className="aboutProject__illustrarion-subtitle">Front-end</p>
         </div>
       </section>
 
       <section
-      className="tech"
+      className="techs"
       >
-        <h2 className="tech__title">Технологии</h2>
-        <p className="tech__subtitle">7 технологий</p>
-        <p className="tech__text">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+        <h2 className="techs__title">Технологии</h2>
+        <p className="techs__subtitle">7 технологий</p>
+        <p className="techs__text">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
 
-        <ul className="tech__list list-style">
-          <li className="tech__list-item">HTML</li>
-          <li className="tech__list-item">CSS</li>
-          <li className="tech__list-item">JS</li>
-          <li className="tech__list-item">React</li>
-          <li className="tech__list-item">Git</li>
-          <li className="tech__list-item">Express.js</li>
-          <li className="tech__list-item">mongoDB</li>
+        <ul className="techs__list list-style">
+          <li className="techs__list-item">HTML</li>
+          <li className="techs__list-item">CSS</li>
+          <li className="techs__list-item">JS</li>
+          <li className="techs__list-item">React</li>
+          <li className="techs__list-item">Git</li>
+          <li className="techs__list-item">Express.js</li>
+          <li className="techs__list-item">mongoDB</li>
         </ul>
 
 
