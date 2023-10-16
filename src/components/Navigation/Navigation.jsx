8f2d81b576ff472f801600aef75908ad88422a16
Nikolai Panteleev <panteleew.kolya@gmail.com>
2023-10-16 19:01:26 +0900

    Изменил поведение сетки фотокарточек наc flex на grid

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -8,8 +8,7 @@ function Navigation() {
   const [openMenu, setOpenMenu] = useState(false);
   const location = useLocation();
 
-  function toggleOpenMenu(event) {
-    event.stopPropagation();
+  function toggleOpenMenu() {
     setOpenMenu(!openMenu);
     if(!openMenu){
       document.body.classList.add('no-scroll')
@@ -24,6 +23,7 @@ function Navigation() {
       let screenWidth = event.target.innerWidth;
       if(screenWidth > 768) {
         setOpenMenu(false);
+        document.body.classList.remove('no-scroll');
       }
     }
     window.addEventListener('resize', handleResize);
@@ -40,12 +40,17 @@ function Navigation() {
       onClick={toggleOpenMenu}
       />
 
+<<<<<<< HEAD
       <nav
         className={`navigation ${openMenu ? 'navigation_oppened' : ''}`}
       >
         <nav
           className='navigation__menu'
         >
+=======
+      <nav className={`navigation ${openMenu ? 'navigation_oppened' : ''}`}>
+        <nav className='navigation__menu'>
+>>>>>>> b658274 (изменил поведение фотокарточек при разных разрешениях)
           <Link to='/' className={`navigation__menu-link link
           ${location.pathname === '/' ? 'navigation__menu-link_active' : ''}`}>Главная</Link>
           <Link to='/movies' className={`navigation__menu-link link
